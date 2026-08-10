8d528b6863b72041eced492c0866074b5f623480
ElizaNorth <eliza.north@outlook.com>
2026-08-11 09:01:09 +1000

    Add delete

--- src/main.js
+++ src/main.js
@@ -13,7 +13,15 @@ const mealNameInput = document.getElementById("meal-name-input");
 const mealTypeInput = document.getElementById("meal-type-input");
 const ingredientsInput = document.getElementById("ingredients-input");
 
-const meals = loadMeals();
+let meals = loadMeals();
+
+function initializeApp(meals) {
+  meals.forEach(function (meal) {
+    renderCard(meal);
+  });
+}
+
+initializeApp(meals);
 
 addMealBtn.addEventListener("click", () => {
   mealForm.classList.add("open");
@@ -32,6 +40,8 @@ mealForm.addEventListener("submit", function (event) {
   mealForm.classList.remove("open");
   overlay.classList.remove("open");
   mealForm.classList.add("hidden");
+
+  this.reset();
 });
 
 cancelBtn.addEventListener("click", function () {
@@ -50,6 +60,20 @@ function createMeal() {
   return newMeal;
 }
 
+function addDeleteListener(deleteBtn, meal, mealCard) {
+  deleteBtn.addEventListener("click", function () {
+    deleteMeal(meal.id);
+    mealCard.remove();
+  });
+}
+
+function deleteMeal(id) {
+  meals = meals.filter(function (meal) {
+    return meal.id !== id;
+  });
+  localStorage.setItem("meals", JSON.stringify(meals));
+}
+
 function saveMeal(meal) {
   meals.push(meal);
   localStorage.setItem("meals", JSON.stringify(meals));
@@ -58,18 +82,27 @@ function saveMeal(meal) {
 function renderCard(meal) {
   const mealCard = document.createElement("div");
   const mealTitle = document.createElement("h2");
+  const cardHeader = document.createElement("div");
   const mealTypeEl = document.createElement("div");
+  const deleteBtn = document.createElement("button");
 
   mealCard.className = "card";
+  cardHeader.className = "card-header";
   mealTypeEl.className = "pill";
+  deleteBtn.className = "delete-btn";
 
   mealTitle.textContent = meal.name;
   mealTypeEl.textContent = meal.type;
+  deleteBtn.textContent = "x";
 
-  mealCard.appendChild(mealTypeEl);
+  mealCard.appendChild(cardHeader);
+  cardHeader.appendChild(mealTypeEl);
+  cardHeader.appendChild(deleteBtn);
   mealCard.appendChild(mealTitle);
 
   cardsContainer.appendChild(mealCard);
+
+  addDeleteListener(deleteBtn, meal, mealCard);
 }
 
 function loadMeals() {
